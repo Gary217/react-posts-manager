af64f7d2c7d0a71d6386930cce0fe80ec6c4d6e1
Alexander Khalizev <Geriuxis@gmail.com>
2026-01-01 15:14:34 +0300

    refactor(styles): move PostFilter inline styles to CSS module

--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -4,10 +4,7 @@ import { MyInput } from './UI/input/MyInput';
 export const PostFilter = ({ children, selectedSort, sortPosts, searchQuery, setSearchQuery }) => {
   return (
     <section className={cl.PostFilter}>
-      <div
-        className={cl.PostFilter__content}
-        style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}
-      >
+      <div className={cl.PostFilter__content}>
         <h2>Sort by:</h2>
         <select name="sort" value={selectedSort} onChange={(e) => sortPosts(e.target.value)}>
           <option value="title">Title</option>
