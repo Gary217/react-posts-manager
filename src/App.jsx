02d0d8f83a2c0498577b72bdc8661ad8e093afd3
Alexander Khalizev <Geriuxis@gmail.com>
2026-01-03 15:00:56 +0300

    feat(router): implement routing and move main logic to Posts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,14 @@
-import { PostForm } from './components/PostForm';
-import { PostList } from './components/PostList';
-import { useEffect, useMemo, useState } from 'react';
-import { MyButton } from './components/UI/button/MyButton';
-import { MyModal } from './components/UI/modal/MyModal';
-import { PostFilter } from './components/PostFilter';
-import PostService from './API/PostService';
-import { MyLoader } from './components/UI/loader/MyLoader';
-import cl from './components/PostFilter.module.css';
-import { getPagesCount } from './utils/pages';
-import { Pagination } from './components/Pagination';
-import { useProcessedPosts } from './hooks/useProcessedPosts';
-import { usePagination } from './hooks/usePagination';
-import { useFetching } from './hooks/useFetching';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { About } from './pages/About';
+import { Posts } from './pages/Posts';
 
-function App() {
-  const [posts, setPosts] = useState([]);
-  const [modal, setModal] = useState(false);
-  const [selectedSort, setSelectedSort] = useState('title');
-  const [searchQuery, setSearchQuery] = useState('');
-  const [totalPagesCount, setTotalPagesCount] = useState(0);
-  const [pageLimit, setPageLimit] = useState(10);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll(pageLimit, pageNumber);
-    setPosts(response.data);
-
-    const totalPostsCount = response.headers['x-total-count'];
-    setTotalPagesCount(getPagesCount(totalPostsCount, pageLimit));
-  });
-
-  useEffect(() => {
-    fetchPosts();
-  }, [pageNumber]);
-
-  const sortPosts = (sortKey) => {
-    setSelectedSort(sortKey);
-    /* setPosts([...posts].sort((a, b) => a[sortKey].localeCompare(b[sortKey]))); */
-  };
-
-  const searchedAndSortedPosts = useProcessedPosts(posts, selectedSort, searchQuery);
-  const pagesArr = usePagination(totalPagesCount);
-
-  const changePage = (page) => {
-    setPageNumber(page);
-  };
-
+export default function App() {
   return (
-    <>
-      <PostFilter
-        selectedSort={selectedSort}
-        sortPosts={sortPosts}
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      >
-        <div className={cl.PostFilter__content}>
-          <MyButton onClick={() => setModal(true)}>Create post</MyButton>
-        </div>
-      </PostFilter>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm posts={posts} setPosts={setPosts} setVisible={setModal} />
-      </MyModal>
-      {isPostLoading ? (
-        <MyLoader />
-      ) : (
-        <PostList
-          posts={searchedAndSortedPosts}
-          setPosts={setPosts}
-          pageNumber={pageNumber}
-          pageLimit={pageLimit}
-          postError={postError}
-        >
-          Posts about React.js
-        </PostList>
-      )}
-      <Pagination pagesArr={pagesArr} pageNumber={pageNumber} changePage={changePage} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
