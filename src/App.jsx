f3c13aeed315e101ea75692f1645cb483550accc
Alexander Khalizev <Geriuxis@gmail.com>
2026-01-04 22:06:17 +0300

    refactor(routing): extract routes into AppRouter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { About } from './pages/About';
-import { Posts } from './pages/Posts';
+import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-import { Error } from './pages/Error';
+import { AppRouter } from './components/AppRouter';
 
 export default function App() {
   return (
@@ -13,12 +11,7 @@ export default function App() {
           { path: 'posts', linkName: 'Posts' },
         ]}
       />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="*" element={<Navigate to="/error" />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   );
 }
