173d5cc83fcd27b32ba06e7da8a50273d3ab14fe
Alexander Khalizev <Geriuxis@gmail.com>
2026-01-09 23:37:24 +0300

    docs(about): update text content to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,11 @@ export const About = () => {
   return (
     <section>
       <h1>About PostManager</h1>
-      <p>Some text...</p>
+      <p style={{ fontSize: '1.2em' }}>
+        PostsManager is a React SPA for managing posts. The project demonstrates skills with the
+        modern React stack: hooks, routing, state management, server interaction, and creating
+        reusable UI components and a component library.
+      </p>
     </section>
   );
 };
